608fc05c82230965899bf6eb2d36006e34640a79
daviabizniz <daviabizniz@gmail.com>
2025-03-11 23:06:34 -0500

    feat(nest-api-tutorial): updated the signup auth service method
    - updated it to accept the dto
    - updated it to save the user to the database
    - updated to return a modified version of the saved user without the password has

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 // import { AuthDto } from './dto';
@@ -10,7 +10,7 @@ export class AuthController {
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     console.log({ dto });
-    return this.authService.signup();
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
-  signup() {
-    return { msg: 'I am signed up' };
+  async signup(dto: AuthDto) {
+    //generate password hash
+    const passwordHash: string = await argon2.hash(dto.password);
+
+    //save new user to the db
+    const user: User = await this.prismaService.user.create({
+      data: { email: dto.email, hash: passwordHash },
+    });
+
+    //remove hash from user being returned
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { hash, ...savedUser } = user;
+
+    //return saved user
+    return savedUser;
   }
 
   signin() {
