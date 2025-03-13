26e8434423d2154ccfa5f4e9ce005a8714d47f4f
daviabizniz <daviabizniz@gmail.com>
2025-03-12 21:33:13 -0500

    feat(nest-api-tutorial): updated the signup method to throw a forbidden error when the prisma error is about unique constraint being violated

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon2 from 'argon2';
 import { User } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
   async signup(dto: AuthDto) {
-    //generate password hash
-    const passwordHash: string = await argon2.hash(dto.password);
+    try {
+      //generate password hash
+      const passwordHash: string = await argon2.hash(dto.password);
 
-    //save new user to the db
-    const user: User = await this.prismaService.user.create({
-      data: { email: dto.email, hash: passwordHash },
-    });
+      //save new user to the db
+      const user: User = await this.prismaService.user.create({
+        data: { email: dto.email, hash: passwordHash },
+      });
 
-    //remove hash from user being returned
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { hash, ...savedUser } = user;
+      //remove hash from user being returned
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { hash, ...savedUser } = user;
 
-    //return saved user
-    return savedUser;
+      //return saved user
+      return savedUser;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      } else {
+        throw error;
+      }
+    }
   }
 
   signin() {
