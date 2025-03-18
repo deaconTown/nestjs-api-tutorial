a5f2203c4e8881cf4668c31de4c20995462cff21
daviabizniz <daviabizniz@gmail.com>
2025-03-17 20:52:44 -0500

    fix(nest-api-tutorial: get GetUser custom decorator not extracting the user data when trying to get the userid from the user in the user controller

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,10 +1,13 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const request = ctx.switchToHttp().getRequest();
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request: Express.Request = ctx.switchToHttp().getRequest();
+    if (data) {
+      if (request.user) {
+        return request.user[data] as string;
+      }
+    }
     return request.user;
   },
 );
